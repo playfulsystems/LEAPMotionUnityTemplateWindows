3a217d07dafc6f8a1367c85f3cb2ee02b935f74b
playfulsystems <44376983+playfulsystems@users.noreply.github.com>
2020-01-27 16:13:12 -0500

    Initial commit

---
+++ Assets/Scripts/ResetOnSpace.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetOnSpace : MonoBehaviour
+{
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            transform.position = startPos;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+}
